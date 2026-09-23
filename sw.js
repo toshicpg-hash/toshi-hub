c5b1e1647e0346a6b7bf6ecd9cd01b4f9f0bff51
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-23 09:55:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8';
+const CACHE='toshi-hub-v8-1';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
