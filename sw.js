c0c762de0277cc809cf4a602b57f3e83eb954c7b
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-23 15:25:01 +0900

    TOSHI HUB v8.3.2 TASK日またぎ・Google重複除去

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.3.1';
+const CACHE='toshi-hub-v8.3.2';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
