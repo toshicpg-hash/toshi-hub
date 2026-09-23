d672dac9b7958060d06c256ae8e1ac4c5483fc34
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-23 21:58:02 +0900

    週表示祝日対応のキャッシュを更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.3.4';
+const CACHE='toshi-hub-v8.3.5';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
