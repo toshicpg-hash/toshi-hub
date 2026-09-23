d7171b86527e667931409b3d150c57b7324744d5
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-23 17:26:35 +0900

    Gmail自動登録のホテル・フライト予定を同期 (#1)
    * Sync Gmail-generated events from primary calendar
    
    * Sync Gmail-generated events from primary calendar
    
    * Sync Gmail-generated events from primary calendar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.3.2';
+const CACHE='toshi-hub-v8.3.3';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
