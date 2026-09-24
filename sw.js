af87f9567420902f3f7f4623188ff7a4b27d6206
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-24 19:45:15 +0900

    PWAキャッシュをv8.4.5へ更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.4.4';
+const CACHE='toshi-hub-v8.4.5';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
