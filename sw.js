0fe86508338db8f3fd97dc5dfc9f7f77d7c77fe5
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-24 21:03:32 +0900

    TOSHI HUB v8.4.7: show all categories in gantt (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.4.6';
+const CACHE='toshi-hub-v8.4.7';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
