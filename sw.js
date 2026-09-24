8b658c523ad319fae079cc863b7c7e6140c8c776
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-24 12:45:41 +0900

    TOSHI HUB v8.4.2: monthly money totals and cash link

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.4.1';
+const CACHE='toshi-hub-v8.4.2';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
