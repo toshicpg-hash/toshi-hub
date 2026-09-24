ddfc01a4ed72f22c532d1b58d2c9ed63f2f4ab40
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-24 12:52:38 +0900

    TOSHI HUB v8.4.3: shrink home cash clearing controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.4.2';
+const CACHE='toshi-hub-v8.4.3';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
