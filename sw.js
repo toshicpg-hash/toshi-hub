2f80f6eb683b2d69222c763044e130185b7d19fd
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-25 10:04:22 +0900

    Refresh TOSHI HUB service worker cache for v8.4.11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.4.10';
+const CACHE='toshi-hub-v8.4.11';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
