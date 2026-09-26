11d4c0dc403bf1c6f56df1345bb1ee8eec9d301c
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-26 15:48:55 +0900

    入出金表示更新のキャッシュを更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.4.36';
+const CACHE='toshi-hub-v8.4.37';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
