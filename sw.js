a6352a9d980b0b939606545bdeb8acfd0d51c19b
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-26 09:22:00 +0900

    Bump TOSHI HUB to v8.4.24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.4.23';
+const CACHE='toshi-hub-v8.4.24';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
