0687cb586b281b46d9f2436efcd8de21a26530a3
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-26 10:10:13 +0900

    キャッシュをv8.4.28へ更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.4.27';
+const CACHE='toshi-hub-v8.4.28';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
