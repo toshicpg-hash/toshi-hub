94f631d52c0fd6e22665a268944f42470a1b6a6b
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-26 16:14:05 +0900

    Preview board documents in place v8.4.39 (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.4.38';
+const CACHE='toshi-hub-v8.4.39';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
