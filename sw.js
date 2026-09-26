c47df38b77500608e67e427218d45d06d18c8439
toshicpg-hash <toshi.cpg@gmail.com>
2026-09-26 16:05:40 +0900

    Add one-time board v8.4.38 (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toshi-hub-v8.4.37';
+const CACHE='toshi-hub-v8.4.38';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
